300a3390b9156542e2884b3722eb2e0bf18bcf62
cavalliniph <paulo.cavallini08@gmail.com>
2026-08-18 06:46:07 -0300

    chore: adiciona cors

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import os
 from flask import Flask
 from flask.cli import load_dotenv
 from view.auth import auth_bp
-import config
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(auth_bp)
 app.config.from_pyfile("config.py")
+
+CORS(app, supports_credentials=True, origins=['http://localhost:5173'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
