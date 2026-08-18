3d872822168ec5f53b81f1421205d1204738fc39
cavalliniph <paulo.cavallini08@gmail.com>
2026-08-18 08:50:45 -0300

    feat: cria esqueleto cadastro

--- funcao.py
+++ funcao.py
@@ -1,4 +1,5 @@
 from flask import current_app
+from flask_bcrypt import check_password_hash, generate_password_hash
 import jwt
 
 def gerar_token(payload) -> str | None:
@@ -11,3 +12,9 @@ def gerar_token(payload) -> str | None:
 		return token
 	except Exception as e:
 		print(f"erro ao criar token: {str(e)}")
+
+def senha_correta(senha_hash, senha):
+	return check_password_hash(senha_hash, senha)
+
+def criar_hash_senha(senha):
+	return generate_password_hash(senha)
--- view/auth.py
+++ view/auth.py
@@ -1,27 +1,67 @@
-from flask import Blueprint, jsonify, make_response
+import random
+from flask import Blueprint, jsonify, make_response, request
 from database.db import con
-from funcao import gerar_token
+from funcao import gerar_token, criar_hash_senha
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/api/auth')
 
 @auth_bp.route('/login', methods=['GET'])
 def login():
 	try:
-		response = make_response({
-			"foo": "bar"
-		})
+		data = request.json() # parseando
+		
+		email = str(data.get('email'))
+		senha = str(data.get('senha'))
 
-		token = gerar_token({ "foo": "sure" })
+		if not email or not senha:
+			return jsonify({ "error": "Email e senha são obrigatorios" }), 400
 
+		cursor = con.cursor()
+		cursor.execute("SELECT * FROM usuario WHERE email = ?", (email,))
+		usuario = cursor.fetchone()
+
+		print(usuario)
+
+		if not usuario:
+			return jsonify({ "error": "Usuario nao encontrado" }), 404
+
+		token = gerar_token(usuario)
+		
 		if not token:
-			return jsonify({ "foo": "bar" })
+			raise RuntimeError("Erro ao gerar token")
+		
+		response = make_response({
+			"message": "Usuario logado com sucesso",
+		})
 
 		response.set_cookie("access_token", token)
 
 		return response
 	except Exception as e:
+		print(str(e))
 		return jsonify({ "error": "Internal server error" }), 500
 
-
+# cadastro de cliente comum
+@auth_bp.route('/cadastro', methods=['POST'])
 def cadastro():
-	return jsonify({}) 
+	try:
+		email = request.form.get('email')
+		nome = request.form.get('nome')
+		telefone = request.form.get('telefone')
+		senha = request.form.get('senha')
+		cpf = request.form.get('cpf')
+
+		if not email or not telefone or not senha or not cpf:
+			return jsonify({ "error": "Todos os campos sao obrigatorios" }), 400
+
+		senha_hash = criar_hash_senha(senha)
+
+		cursor = con.cursor()
+		codigo = codigo = random.randint(100000, 999999)
+		cursor.execute("INSERT INTO usuario (nome, email, telefone, senha, cpf, codigo, usuario_role) VALUES (?, ?, ?, ?, ?, ?, ?)",
+				 (nome, email, telefone, senha_hash, cpf, codigo, 'PACIENTE'))
+
+		return jsonify({ "message": "Usuario cadastrado com sucesso" }), 201
+	except Exception as e:
+		print(f"houve um erro ao realizar o cadastro: {str(e)}")
+		return jsonify({ "error": "Internal server error" }), 500
