ca813284da87b22b158c32a5c1c26ba64b68da1d
cavalliniph <paulo.cavallini08@gmail.com>
2026-09-01 13:03:14 -0300

    feat(registros): cria endpoint get
    cria rota de todos os registros (autenticacao pendente)

--- view/registros.py
+++ view/registros.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, jsonify
+from database.db import con
 
 registros_bp = Blueprint('registros', __name__, url_prefix='/api/registros')
 
 @registros_bp.route('/', methods=['GET'])
 def registros():
-    return jsonify({ 'messsage': 'Hello registros' })
+    cur = con.cursor()
+
+    try:
+
+        cur.execute('SELECT * FROM registro')
+        resultados = cur.fetchall()
+        columns = [col[0].lower() for col in cur.description]
+
+        registros = [dict(zip(columns, row)) for row in resultados]
+
+        return jsonify({ 'registros': registros })
+    except Exception as e:
+        print(f"erro ao contar registros: {str(e)}")
+        return jsonify({ 'error': 'Internal Server Error' }), 500
 
